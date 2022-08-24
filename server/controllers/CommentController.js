3ff1756d186fcc11068060b485d115c8088cca8e
belfortep <pbelforte2003@gmail.com>
2022-08-24 07:08:58 -0300

    added comments controller

--- server/controllers/CommentController.js
+++ server/controllers/CommentController.js
@@ -1,8 +1,65 @@
 import HttpCodesEnum from '../enum/httpCodes.js'
-
+import Comment from '../models/Comment.js'
+import Video from '../models/Video.js'
+import { createError } from '../error.js';
 class CommentController {
 
+    static async addComment(req, res, next) {
+        try {
 
+            const comment = new Comment({ ...req.body, userId: req.user.id })
+
+            await comment.save()
+
+            return res.status(HttpCodesEnum.CREATED).json(comment);
+
+        } catch (err) {
+
+            return next(err);
+
+        }
+    }
+
+    static async deleteComment(req, res, next) {
+
+        try {
+
+            const comment = await Comment.findById(res.params.id)
+
+            const video = await Video.findById(res.params.id)
+
+            if (verifyOwner(comment.userId, req.user.id) || verifyOwner(req.user.id, video.userId)) {
+
+                await Comment.findByIdAndDelete(req.params.id)
+
+                return res.status(HttpCodesEnum.OK).json('Comment deleted')
+
+            } else {
+
+                return next(createError(HttpCodesEnum.FORBBIDEN, 'You can only delete your comments'))
+
+            }
+
+        } catch (err) {
+
+            return next(err);
+
+        }
+    }
+
+    static async getComments(req, res, next) {
+        try {
+
+            const comments = await Comment.find({ videoId: req.params.videoId })
+
+            return res.status(HttpCodesEnum.OK).json(comments)
+
+        } catch (err) {
+
+            return next(err);
+
+        }
+    }
 
 }
 
--- server/routes/comments.js
+++ server/routes/comments.js
@@ -1,6 +1,13 @@
 import express from 'express'
 const router = express.Router();
 import CommentController from '../controllers/CommentController.js'
+import { verifyToken } from '../verifyToken.js';
+
+router.post('/', verifyToken, CommentController.addComment)
+
+router.delete('/:id', verifyToken, CommentController.deleteComment)
+
+router.get('/:videoId', CommentController.getComments)
 
 
 
